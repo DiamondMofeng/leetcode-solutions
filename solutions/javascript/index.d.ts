b0465180f6b108cf37864e180cb69eb8aee754a1
Mofeng <363034937@qq.com>
2024-11-24 20:22:16 +0800

    chore(js): add PriorityQueue type declaration

--- solutions/javascript/index.d.ts
+++ solutions/javascript/index.d.ts
@@ -1,1 +1,30 @@
 declare const _: import('lodash').LoDashStatic
+
+interface PriorityQueueOptions<T> {
+  priority?: (element: T) => number;
+  compare?: (a: T, b: T) => number;
+}
+
+declare class PriorityQueue<T> {
+  constructor(options?: PriorityQueueOptions<T>);
+  size(): number;
+  isEmpty(): boolean;
+  front(): T;
+  back(): T;
+  enqueue(element: T, priority?: number): PriorityQueue<T>;
+  dequeue(): T;
+  toArray(): T[];
+  clear(): void;
+}
+
+declare class MaxPriorityQueue<T> extends PriorityQueue<T> {
+  static from<T>(
+    entries: Readonly<Iterable<readonly [element: T, priority: number]>>
+  ): MaxPriorityQueue<T>;
+}
+
+declare class MinPriorityQueue<T> extends PriorityQueue<T> {
+  static from<T>(
+    entries: Readonly<Iterable<readonly [element: T, priority: number]>>
+  ): MinPriorityQueue<T>;
+}
